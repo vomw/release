d24587eeda22b11ab4552ba3bd8a4755f6554945
vomw <22770640+vomw@users.noreply.github.com>
2024-04-17 19:15:22 +0800

    remove

--- src/main.c
+++ src/main.c
@@ -1349,7 +1349,7 @@ main_scan(struct Masscan *masscan)
         now = time(0);
         safe_gmtime(&x, &now);
         strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S GMT", &x);
-        LOG(0, "Starting masscan " MASSCAN_VERSION " (http://bit.ly/14GZzcT) at %s\n",
+        LOG(0, "Starting masscan " MASSCAN_VERSION " at %s\n",
             buffer);
 
         if (count_ports == 1 && \
