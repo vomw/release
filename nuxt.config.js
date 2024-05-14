dfb030e270cbb65d3e30f1994d46372ca17e521a
vomw <22770640+vomw@users.noreply.github.com>
2024-05-14 13:38:25 +0800

    2024-05-14 13:38

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,88 +1,96 @@
-const path = require("path");
+// const path = require("path");
 
-module.exports = {
-  plugins: ["~plugins/vuetify.js", "~plugins/ymap.js"],
-  modules: ["@nuxtjs/markdownit", "@nuxtjs/sitemap"],
+// module.exports = {
+//   plugins: ["~plugins/vuetify.js", "~plugins/ymap.js"],
+//   modules: ["@nuxtjs/markdownit", "@nuxtjs/sitemap"],
 
-  target: 'static',
+//   target: 'static',
 
-  sitemap: {
-    hostname: "",
-    gzip: false
-  },
+//   sitemap: {
+//     hostname: "",
+//     gzip: false
+//   },
 
-  css: [{
-    src: path.join(__dirname, "assets/css/app.styl"),
-    lang: "styl"
-  }],
+//   css: [{
+//     src: path.join(__dirname, "assets/css/app.styl"),
+//     lang: "styl"
+//   }],
 
-  // [optional] markdownit options
-  // See https://github.com/markdown-it/markdown-it
-  markdownit: {
-    preset: "default",
-    linkify: true,
-    breaks: true,
-    html: true,
-    typographer: true,
-    use: ["markdown-it-container", "markdown-it-attrs", "markdown-it-anchor"]
-  },
+//   // [optional] markdownit options
+//   // See https://github.com/markdown-it/markdown-it
+//   markdownit: {
+//     preset: "default",
+//     linkify: true,
+//     breaks: true,
+//     html: true,
+//     typographer: true,
+//     use: ["markdown-it-container", "markdown-it-attrs", "markdown-it-anchor"]
+//   },
 
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: "DNSCrypt version 2 - Official Project Home Page",
-    htmlAttrs: {
-      lang: "en"
-    },
-    meta: [{
-      charset: "utf-8"
-    },
-    {
-      name: "viewport",
-      content: "width=device-width, initial-scale=1"
-    },
-    {
-      hid: "description",
-      name: "description",
-      content: "New home of the DNSCrypt project, now implementing multiple protocols to improve DNS security. Download official DNSCrypt & DoH servers and clients here."
-    }
-    ]
-  },
-  /*
-   ** Customize the progress bar color
-   */
-  loading: {
-    color: "#3B8070"
-  },
-  /*
-   ** Build configuration
-   */
-  build: {
-    /*
-     ** Run ESLint on save
-     */
-    extend(config, {
-      isDev,
-      isClient
-    }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: "pre",
-          test: /\.(js|vue)$/,
-          loader: "eslint-loader",
-          exclude: /(node_modules)/
-        });
-      }
-    }
-  }
-};
+//   /*
+//    ** Headers of the page
+//    */
+//   head: {
+//     title: "DNSCrypt version 2 - Official Project Home Page",
+//     htmlAttrs: {
+//       lang: "en"
+//     },
+//     meta: [{
+//       charset: "utf-8"
+//     },
+//     {
+//       name: "viewport",
+//       content: "width=device-width, initial-scale=1"
+//     },
+//     {
+//       hid: "description",
+//       name: "description",
+//       content: "New home of the DNSCrypt project, now implementing multiple protocols to improve DNS security. Download official DNSCrypt & DoH servers and clients here."
+//     }
+//     ]
+//   },
+//   /*
+//    ** Customize the progress bar color
+//    */
+//   loading: {
+//     color: "#3B8070"
+//   },
+//   /*
+//    ** Build configuration
+//    */
+//   build: {
+//     /*
+//      ** Run ESLint on save
+//      */
+//     extend(config, {
+//       isDev,
+//       isClient
+//     }) {
+//       if (isDev && isClient) {
+//         config.module.rules.push({
+//           enforce: "pre",
+//           test: /\.(js|vue)$/,
+//           loader: "eslint-loader",
+//           exclude: /(node_modules)/
+//         });
+//       }
+//     }
+//   }
+// };
 
 
 
-export default {
-  target: 'static',
-  router: {
-    base: '/sanctioned/'
+// export default {
+//   target: 'static',
+//   router: {
+//     base: '/sanctioned/'
+//   }
+// }
+
+
+export default defineNuxtConfig({
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/sanctioned/' : '/',
+    buildAssetsDir: '/static/'
   }
 }
