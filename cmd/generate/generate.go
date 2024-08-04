4e2611b9a902e72e8f3541e52e3462cfa3b554e2
vomw <22770640+vomw@users.noreply.github.com>
2024-08-04 18:16:48 +0800

    wgcf

---
+++ cmd/generate/generate.go
@@ -0,0 +1,66 @@
+package generate
+
+import (
+	"log"
+
+	"github.com/ViRb3/wgcf/cloudflare"
+	. "github.com/ViRb3/wgcf/cmd/shared"
+	"github.com/ViRb3/wgcf/config"
+	"github.com/ViRb3/wgcf/util"
+	"github.com/ViRb3/wgcf/wireguard"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var profileFile string
+var shortMsg = "Generates a WireGuard profile from the current Cloudflare Warp account"
+
+var Cmd = &cobra.Command{
+	Use:   "generate",
+	Short: shortMsg,
+	Long:  FormatMessage(shortMsg, ``),
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := generateProfile(); err != nil {
+			log.Fatal(util.GetErrorMessage(err))
+		}
+	},
+}
+
+func init() {
+	Cmd.PersistentFlags().StringVarP(&profileFile, "profile", "p", "wgcf-profile.conf", "WireGuard profile file")
+}
+
+func generateProfile() error {
+	if !IsConfigValidAccount() {
+		return errors.New("no account detected")
+	}
+
+	ctx := CreateContext()
+	thisDevice, err := cloudflare.GetSourceDevice(ctx)
+	if err != nil {
+		return err
+	}
+	boundDevice, err := cloudflare.GetSourceBoundDevice(ctx)
+	if err != nil {
+		return err
+	}
+
+	profile, err := wireguard.NewProfile(&wireguard.ProfileData{
+		PrivateKey: viper.GetString(config.PrivateKey),
+		Address1:   thisDevice.Config.Interface.Addresses.V4,
+		Address2:   thisDevice.Config.Interface.Addresses.V6,
+		PublicKey:  thisDevice.Config.Peers[0].PublicKey,
+		Endpoint:   thisDevice.Config.Peers[0].Endpoint.Host,
+	})
+	if err != nil {
+		return err
+	}
+	if err := profile.Save(profileFile); err != nil {
+		return err
+	}
+
+	PrintDeviceData(thisDevice, boundDevice)
+	log.Println("Successfully generated WireGuard profile:", profileFile)
+	return nil
+}
---
+++ wireguard/profile.go
@@ -0,0 +1,56 @@
+package wireguard
+
+import (
+	"bytes"
+	"io/ioutil"
+	"text/template"
+)
+
+var profileTemplate = `[Interface]
+PrivateKey = {{ .PrivateKey }}
+Address = {{ .Address1 }}/32
+Address = {{ .Address2 }}/128
+DNS = 1.1.1.1, 1.0.0.1, 2606:4700:4700::1111, 2606:4700:4700::1001
+MTU = 1280
+[Peer]
+PublicKey = {{ .PublicKey }}
+AllowedIPs = 0.0.0.0/0
+AllowedIPs = ::/0
+Endpoint = {{ .Endpoint }}
+`
+
+type Profile struct {
+	profileString string
+}
+
+type ProfileData struct {
+	PrivateKey string
+	Address1   string
+	Address2   string
+	PublicKey  string
+	Endpoint   string
+}
+
+func NewProfile(data *ProfileData) (*Profile, error) {
+	profileString, err := generateProfile(data)
+	if err != nil {
+		return nil, err
+	}
+	return &Profile{profileString: profileString}, nil
+}
+
+func generateProfile(data *ProfileData) (string, error) {
+	t, err := template.New("").Parse(profileTemplate)
+	if err != nil {
+		return "", err
+	}
+	var result bytes.Buffer
+	if err := t.Execute(&result, data); err != nil {
+		return "", err
+	}
+	return result.String(), nil
+}
+
+func (p *Profile) Save(profileFile string) error {
+	return ioutil.WriteFile(profileFile, []byte(p.profileString), 0600)
+}
